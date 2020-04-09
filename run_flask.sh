644ad07033637b0c8ab9e2a6427399a302302801
Li-Hsin Chang <li-hsin.l.chang@utu.fi>
2020-04-09 20:56:11 +0300

    run on server

--- run_flask.sh
+++ run_flask.sh
@@ -1,4 +1,17 @@
+# on puhti
+source /projappl/project_2002820/venv/annotool/bin/activate
+
+# this one required for epsilon
+#export PYTHONPATH=/home/lhchan/para-anno/
+
 export FLASK_ENV=development
 export FLASK_APP=paraanno.app
-export PARAANN_DATA=/home/ginter/newsdata
-flask run
+export PARAANN_DATA=/scratch/project_2002820/lihsin/para-data
+
+if [ "$#" -eq 1 ]; then
+    PORT=$1
+    flask run --port $PORT
+else
+    flask run
+fi
+
