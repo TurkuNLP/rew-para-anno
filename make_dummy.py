70e8f34b4cbb36521496aebcc323e0db556c1adf
Filip Ginter <ginter@cs.utu.fi>
2020-09-01 18:41:48 +0300

    make dummy json

---
+++ make_dummy.py
@@ -0,0 +1,7 @@
+import json
+
+data=[{"txt1":"Some text 1","txt2":"Some text 2","metadata":"something"},{"txt1":"Some text 3","txt2":"Some text 4","metadata":"something"},{"txt1":"Some text 5","txt2":"Some text 6","metadata":"something"},{"txt1":"Some text 7","txt2":"Some text 8","metadata":"something"},{"txt1":"Some text 9","txt2":"Some text 10","metadata":"something"},{"txt1":"Some text 11","txt2":"Some text 12","metadata":"something"}]
+
+s=json.dumps(data,ensure_ascii=False,indent=2,sort_keys=True)
+print(s)
+
