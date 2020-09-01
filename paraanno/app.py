e890b36c7f665ef3f5aac2cc429cce21027c6bbe
Filip Ginter <ginter@cs.utu.fi>
2020-09-01 18:41:38 +0300

    change to text pairs

--- paraanno/app.py
+++ paraanno/app.py
@@ -13,68 +13,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 DATADIR=os.environ["PARAANN_DATA"]
 
-def matches(s1,s2,minlen=5):
-    m=difflib.SequenceMatcher(None,s1,s2,autojunk=False)
-
-    #returns list of (idx1,idx2,len) perfect matches
-    return sorted(matches_r(m,s1,s2,minlen,0,len(s1),0,len(s2)),
-                  key=lambda match: (match[2], match[0]))
-
-def matches_r(m,s1,s2,min_len,s1_beg,s1_end,s2_beg,s2_end):
-    lm=m.find_longest_match(s1_beg,s1_end,s2_beg,s2_end)
-    #with open("log.txt","a") as f:
-        #print(">>>> CHECKED",file=f)
-        #print(s1[s1_beg:s1_end],file=f)
-        #print("---",file=f)
-        #print(s2[s2_beg:s2_end],file=f)
-        #print(">>>> FOUND",file=f)
-        #print(s1[lm.a:lm.a+lm.size],file=f)
-        #print("\n",file=f)
-    #s1[lm.a:lm.a+lm.size]
-    #s2[lm.b:lm.b+lm.size]
-    if lm.size<min_len:
-        return []
-    else:
-        s1_left=s1_beg,lm.a
-        s1_right=lm.a+lm.size,s1_end
-        s1_all=(s1_beg,s1_end)
-        
-        s2_left=s2_beg,lm.b
-        s2_right=lm.b+lm.size,s2_end
-        s2_all=(s2_beg,s2_end)
-        
-        matches=[(lm.a,lm.b,lm.size)]
-        for i1,i2 in ((s1_left,s2_left),(s1_left,s2_right),(s1_right,s2_left),(s1_right,s2_right)):
-            #try all combinations of what remains
-            if i1[1]-i1[0]<min_len:
-                continue #too short to produce match
-            if i2[1]-i2[0]<min_len:
-                continue #too short to produce match
-            sub=matches_r(m,s1,s2,min_len,*i1,*i2)
-            matches.extend(sub)
-        return matches
-
-def build_spans(s,blocks):
-    """s:string, blocks are pairs of (idx,len) of perfect matches"""
-    #allright, this is pretty dumb alg!
-    matched_indices=[0]*len(s)
-    for i,l in blocks:
-        for idx in range(i,i+l):
-            matched_indices[idx]=max(matched_indices[idx],l)
-    spandata=[]
-    for c,matched_len in zip(s,matched_indices):
-        #matched_len=(matched_len//5)*5
-        if not spandata or spandata[-1][1]!=matched_len: #first or span with opposite match polarity -> must make new!
-            spandata.append(([],matched_len))
-        spandata[-1][0].append(c)
-    merged_spans=[(html.escape("".join(chars)),matched_len) for chars,matched_len in spandata]
-    return merged_spans, min(matched_indices),max(matched_indices) #min is actually always 0, but it's here for future need
-    
-    
-
-#matches("Minulla on koira mutta sinulla on kissa.","Sinulla on kissa ja minulla on koira.")
-
-
 def read_batches():
     batchdict={} #user -> batchfile -> Batch
     batchfiles=sorted(glob.glob(DATADIR+"/batches-*/*.json"))
@@ -90,7 +28,7 @@ class Batch:
     def __init__(self,batchfile):
         self.batchfile=batchfile
         with open(batchfile) as f:
-            self.data=json.load(f) #this is a list of document pairs to annotate
+            self.data=json.load(f) #this is a list of sentence pairs to annotate
 
     def save(self):
         s=json.dumps(self.data,ensure_ascii=False,indent=2,sort_keys=True)
@@ -99,12 +37,7 @@ class Batch:
         
 def init():
     global all_batches
-    global textdbs
     all_batches=read_batches()
-    textdbs={}
-    for src in SqliteDict.get_tablenames(DATADIR+"/all-texts.sqlited"):
-        textdbs[src]=SqliteDict(DATADIR+"/all-texts.sqlited",tablename=src)
-    print(list(textdbs.keys()))
 
 init()            
 
@@ -121,15 +54,12 @@ def batchlist(user):
 @app.route("/ann/<user>/<batchfile>")
 def jobsinbatch(user,batchfile):
     global all_batches
-    global textdbs
     pairs=all_batches[user][batchfile].data
     pairdata=[]
     for idx,pair in enumerate(pairs):
-        src1,f1=pair["d1"]
-        src2,f2=pair["d2"]
-        text1=textdbs[src1][f1]
-        text2=textdbs[src2][f2]
-        pairdata.append((idx,pair.get("updated","not updated"),text1[:100],text2[:100]))
+        text1=pair["txt1"]
+        text2=pair["txt2"]
+        pairdata.append((idx,pair.get("updated","not updated"),text1[:50],text2[:50]))
     return render_template("doc_list_in_batch.html",user=user,batchfile=batchfile,pairdata=pairdata)
 
 @app.route("/saveann/<user>/<batchfile>/<pairseq>",methods=["POST"])
@@ -146,7 +76,6 @@ def save_document(user,batchfile,pairseq):
 @app.route("/ann/<user>/<batchfile>/<pairseq>")
 def fetch_document(user,batchfile,pairseq):
     global all_batches
-    global textdbs
     pairseq=int(pairseq)
     pair=all_batches[user][batchfile].data[pairseq]
     
@@ -162,17 +91,11 @@ def fetch_document(user,batchfile,pairseq):
     # "sim": 0.9922545481090577
     # }
 
-    src1,f1=pair["d1"]
-    src2,f2=pair["d2"]
-    text1=textdbs[src1][f1]
-    text2=textdbs[src2][f2]
+    text1=pair["txt1"]
+    text2=pair["txt2"]
 
-    blocks=matches(text1,text2,15) #matches are (idx1,idx2,len)
-    spandata1,min1,max1=build_spans(text1,list((b[0],b[2]) for b in blocks))
-    spandata2,min2,max2=build_spans(text2,list((b[1],b[2]) for b in blocks))
-    
-    
-    annotation=pair.get("annotation",[])
-    
-    return render_template("doc.html",left_text=text1,right_text=text2,left_spandata=spandata1,right_spandata=spandata2,pairseq=pairseq,batchfile=batchfile,user=user,annotation=annotation,min_mlen=min(min1,min2),max_mlen=max(max1,max2)+1,mlenv=min(max(max1,max2),30),is_last=(pairseq==len(all_batches[user][batchfile].data)-1))
+    annotation=pair.get("annotation",{})
+
+    labels=[("4","Paraphrase"), ("3","Near paraphrase"), ("2","Related but not paraphrase"), ("1","Unrelated")]
+    return render_template("doc.html",text1=text1,text2=text2,pairseq=pairseq,batchfile=batchfile,user=user,annotation=annotation,labels=labels,is_last=(pairseq==len(all_batches[user][batchfile].data)-1))
 
