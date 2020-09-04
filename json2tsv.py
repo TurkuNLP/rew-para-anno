d3b49bddb26681fc77610066501c29e950094cd2
Jenna Kanerva <jmnybl@utu.fi>
2020-09-04 15:24:43 +0300

    json2tsv

---
+++ json2tsv.py
@@ -0,0 +1,43 @@
+import json
+import argparse
+import glob
+import os
+# TSV fields: ID, annotator, timestamp, label, text
+
+def read_dirs(args):
+    json_files = glob.glob(os.path.join(args.directory, "**", "*.json"), recursive=True)
+    return json_files
+
+def yield_from_json(fname):
+
+    with open(fname, "rt", encoding="utf-8") as f:
+        data = json.load(f)
+    for example in data: # one annotated example
+        annotation = example.get("annotation", None)
+        if not annotation:
+            continue
+        label = annotation.get("label", None)
+        if not label:
+            continue
+        text = " --- ".join([example.get("txt1", "EMPTY"), example.get("txt2", "EMPTY")])
+        yield [example.get("id"), annotation.get("user"), annotation.get("updated"), label, text]
+
+
+def main(args):
+
+    files = read_dirs(args)
+
+    for fname in files:
+        for annotation in yield_from_json(fname):
+            print("\t".join(annotation))
+
+
+
+
+if __name__=="__main__":
+
+    argparser = argparse.ArgumentParser(description='')
+    argparser.add_argument('--directory', '-d', help='Top level directory of annotation batches (.json), will read all subdirectories as well.')
+    args = argparser.parse_args()
+
+    main(args)
