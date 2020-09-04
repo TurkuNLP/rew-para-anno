9449ece846db4c81b9e342ba359fd674ee169d66
Filip Ginter <filip.ginter@gmail.com>
2020-09-04 11:23:40 +0300

    run_flask to dummy

--- run_flask.sh
+++ run_flask.sh
@@ -4,7 +4,7 @@ source /home/ginter/venv-para/bin/activate
 export FLASK_ENV=development
 export FLASK_APP=paraanno.app
 export PARAANN_DATA=$HOME/rew-para-anno/dummy
-export APP_ROOT=/rew-para
+export PARAANN_APP_ROOT=/rew-para
 
 flask run --port 6666
 
