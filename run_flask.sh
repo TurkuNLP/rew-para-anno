cad4b4f5d16ab1eca649b9dada891d040376b6ce
fshdnc <lhchan@utu.fi>
2020-09-04 10:51:42 +0300

    fix overlapping buttons, remove extra file

--- run_flask.sh
+++ run_flask.sh
@@ -1,20 +1,10 @@
-# on puhti
-source /projappl/project_2002820/venv/annotool/bin/activate
-
-# this one required for epsilon
-#export PYTHONPATH=/home/lhchan/para-anno/
+# on pouta
+source /home/ginter/venv-para/bin/activate
 
 export FLASK_ENV=development
 export FLASK_APP=paraanno.app
-export PARAANN_DATA=/scratch/project_2002820/lihsin/para-data
+export PARAANN_DATA=$HOME/rew-para-anno/dummy
+export APP_ROOT=/rew-para
 
-if [ "$#" -eq 1 ]; then
-    PORT=$1
-    flask run --port $PORT
-else
-    flask run
-fi
-
-export PARAANN_DATA=$HOME/Turku-paraphrase-corpus/para-subtitles
-flask run
+flask run --port 6666
 
