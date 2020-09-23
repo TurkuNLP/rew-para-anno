0c0462cc46712c48b1c9d57e420b1dc9fe8c0472
Jenna Kanerva <jmnybl@utu.fi>
2020-09-23 14:59:26 +0300

    tmp context

--- paraanno/app.py
+++ paraanno/app.py
@@ -155,4 +155,76 @@ def fetch_document(user,batchfile,pairseq):
     annotation=pair.get("annotation",{})
     
     return render_template("doc.html",app_root=APP_ROOT,text1=text1,text2=text2,pairseq=pairseq,batchfile=batchfile,user=user,annotation=annotation,is_last=(pairseq==len(all_batches[user][batchfile].data)-1))
+    
+    
+    
+@app.route("/ann/<user>/<batchfile>/<pairseq>/context")
+def fetch_context(user,batchfile,pairseq):
+    global all_batches
+    pairseq=int(pairseq)
+    pair=all_batches[user][batchfile].data[pairseq]
+
+    text1=pair.get("document_context1", "")
+    text2=pair.get("document_context2", "")
+
+    blocks=matches(text1,text2,15) #matches are (idx1,idx2,len)
+    spandata1,min1,max1=build_spans(text1,list((b[0],b[2]) for b in blocks))
+    spandata2,min2,max2=build_spans(text2,list((b[1],b[2]) for b in blocks))
+    
+    
+    return render_template("context.html", app_root=APP_ROOT, left_text=text1, right_text=text2, left_spandata=spandata1, right_spandata=spandata2, pairseq=pairseq, batchfile=batchfile, user=user, min_mlen=min(min1,min2), max_mlen=max(max1,max2)+1, mlenv=min(max(max1,max2),30), is_last=(pairseq==len(all_batches[user][batchfile].data)-1), focus_left=pair.get("focus1", ""), focus_right=pair.get("focus2", ""))
+    
+   
+
+
+
+def matches(s1,s2,minlen=5):
+    m=difflib.SequenceMatcher(None,s1,s2,autojunk=False)
+
+    #returns list of (idx1,idx2,len) perfect matches
+    return sorted(matches_r(m,s1,s2,minlen,0,len(s1),0,len(s2)), key=lambda match: (match[2], match[0]))
+
+def matches_r(m,s1,s2,min_len,s1_beg,s1_end,s2_beg,s2_end):
+    lm=m.find_longest_match(s1_beg,s1_end,s2_beg,s2_end)
+    if lm.size<min_len:
+        return []
+    else:
+        s1_left=s1_beg,lm.a
+        s1_right=lm.a+lm.size,s1_end
+        s1_all=(s1_beg,s1_end)
+        
+        s2_left=s2_beg,lm.b
+        s2_right=lm.b+lm.size,s2_end
+        s2_all=(s2_beg,s2_end)
+        
+        matches=[(lm.a,lm.b,lm.size)]
+        for i1,i2 in ((s1_left,s2_left),(s1_left,s2_right),(s1_right,s2_left),(s1_right,s2_right)):
+            #try all combinations of what remains
+            if i1[1]-i1[0]<min_len:
+                continue #too short to produce match
+            if i2[1]-i2[0]<min_len:
+                continue #too short to produce match
+            sub=matches_r(m,s1,s2,min_len,*i1,*i2)
+            matches.extend(sub)
+        return matches
+
+def build_spans(s,blocks):
+    """s:string, blocks are pairs of (idx,len) of perfect matches"""
+    if not blocks:
+        return [], 0, 0
+    #allright, this is pretty dumb alg!
+    matched_indices=[0]*len(s)
+    for i,l in blocks:
+        for idx in range(i,i+l):
+            matched_indices[idx]=max(matched_indices[idx],l)
+    spandata=[]
+    for c,matched_len in zip(s,matched_indices):
+        #matched_len=(matched_len//5)*5
+        if not spandata or spandata[-1][1]!=matched_len: #first or span with opposite match polarity -> must make new!
+            spandata.append(([],matched_len))
+        spandata[-1][0].append(c)
+    merged_spans=[(html.escape("".join(chars)),matched_len) for chars,matched_len in spandata]
+    return merged_spans, min(matched_indices),max(matched_indices) #min is actually always 0, but it's here for future need
+
+#matches("Minulla on koira mutta sinulla on kissa.","Sinulla on kissa ja minulla on koira.")
 
